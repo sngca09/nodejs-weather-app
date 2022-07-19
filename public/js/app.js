42bec3e24b40b9c0816e223bec1bf28425429471
Anakha TA <sngca09@sngce.ac.in>
2022-07-19 14:43:30 +0530

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-
-
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
@@ -11,7 +9,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location=search.value
     messageOne.textContent='Loading...!'
     meassageTwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error
